a8e66d812ab045ec914110cff88ceb4ef492e291
madelene trang <madelene@helicon.ai>
2023-04-26 12:00:04 +0300

    Create single page listing, the search page and fix background image.

--- listings/views.py
+++ listings/views.py
@@ -1,8 +1,9 @@
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 import listings
 
+from .choices import bedroom_choices, price_choices, state_choices
 from .models import Listing
 
 
@@ -11,13 +12,22 @@ def index(request):
     paginator = Paginator(listings, 6)
     page = request.GET.get("page")
     paged_listings = paginator.get_page(page)
-    context = {"listings": paged_listings}
+    context = {
+        "listings": paged_listings,
+    }
     return render(request, "listings/listings.html", context)
 
 
 def listing(request, listing_id):
-    return render(request, "listings/listing.html")
+    listing = get_object_or_404(Listing, pk=listing_id)
+    context = {"listing": listing}
+    return render(request, "listings/listing.html", context)
 
 
 def search(request):
-    return render(request, "listings/search.html")
+    context = {
+        "state_choices": state_choices,
+        "bedroom_choices": bedroom_choices,
+        "price_choices": price_choices,
+    }
+    return render(request, "listings/search.html", context)
